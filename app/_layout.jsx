20ec18b33e8e2b69846f77f104b7103594ca7648
JUlian-co <hi@juliansinn.de>
2026-05-24 20:59:46 +0200

    session check in root layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,7 +3,7 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
-import { Stack } from "expo-router";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import "react-native-reanimated";
 import "./global.css";
@@ -13,19 +13,37 @@ import { SplashScreenController } from "@/components/splash-screen-controller";
 import { useAuthContext } from "@/hooks/use-auth-context";
 import { useColorScheme } from "@/hooks/use-color-scheme";
 import AuthProvider from "@/providers/auth-provider";
+import { useEffect } from "react";
 
 // Separate RootNavigator so we can access the AuthContext
 function RootNavigator() {
-  const { isLoggedIn } = useAuthContext();
+  // const { isLoggedIn } = useAuthContext(); ist da
+  const { session, isLoading } = useAuthContext();
+  console.log("Session in RootLayoutNav:", session);
+  const segments = useSegments();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    const inAuthGroup = segments[0] === "(auth)";
+
+    if (!session && !inAuthGroup) {
+      router.replace("/(auth)");
+    } else if (session && inAuthGroup) {
+      router.replace("/(tabs)");
+    }
+  }, [session, isLoading, segments]);
 
   return (
     <Stack>
-      <Stack.Protected guard={isLoggedIn}>
+      {/* <Stack.Protected guard={isLoggedIn}> */}
+      <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack.Protected>
-      <Stack.Protected guard={!isLoggedIn}>
-        <Stack.Screen name="login" options={{ headerShown: false }} />
-      </Stack.Protected>
+      </Stack>
+      <Stack>
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      </Stack>
       <Stack.Screen name="+not-found" />
     </Stack>
   );
