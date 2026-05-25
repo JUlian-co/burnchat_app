93451ce2e48f73794c4cb09091dc7e7f7086dc01
JUlian-co <hi@juliansinn.de>
2026-05-25 15:17:21 +0200

    saving user in db

--- app/(auth)/index.jsx
+++ app/(auth)/index.jsx
@@ -2,6 +2,7 @@
 import { useRouter } from "expo-router";
 import { useState } from "react";
 import {
+    Alert,
   Button,
   Platform,
   Text,
@@ -13,20 +14,28 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { supabase } from "../../lib/supabase";
 
 export default function HomeScreen() {
-  const [date, setDate] = useState(new Date(2001, 0, 6));
-  const [show, setShow] = useState(false);
+  const [user, setUser] = useState({ username: null, displayname: null})
   const router = useRouter();
 
-  console.log(date);
+  console.log(user);
 
-  const nextStep = () => {
-    router.push({ pathname: "/name", params: { date } });
-  };
+ /*  const nextStep = () => {
+    router.push({ pathname: "/name", params: { nig: "hi" } });
+  }; */
+
+  const handleUserChange = (e, type) => {
+    setUser({ ...user, [type]: e });
+  }
 
   const handleAnonSignIn = async () => {
     const { data: authData, error: authError } = await supabase.auth.signInAnonymously();
     
-    console.log("authData", authData);
+    console.warn("authData", authData);
+
+    if (!authData?.user.id) {
+      Alert.alert("Auth Fehler", "Keine User ID erhalten");
+      return;
+    }
     console.log("auth user id: ", authData.user.id);
     console.log("authError", authError);
     
@@ -34,15 +43,36 @@ export default function HomeScreen() {
       Alert.alert("Auth Fehler", authError.message);
       return;
     }
+
+
+    const { /* data: profileData, */ error: profileError } = await supabase
+      .from("users")
+      .insert({
+        id: authData.user.id,
+        username: user.username,
+        display_name: user.displayname,
+      })
+      /* .select()
+      .single(); */
+
+    // console.log("profileData", profileData);
+    console.log("profileError", profileError);
+
+    if (profileError) {
+      Alert.alert("Profil Fehler", profileError.message);
+      return;
+    }
+
+    // nextStep();
   }
 
-  const onChange = (event, selectedDate) => {
-    setShow(Platform.OS === "ios"); // iOS bleibt offen, Android schließt nach Auswahl
-    if (selectedDate) setDate(selectedDate);
-  };
 
   return (
     <SafeAreaView className="flex-1 justify-center items-center px-4 bg-black">
+
+        <TextInput placeholder="Nutzername" placeholderTextColor="#888" className="bg-gray-800 text-white p-4 rounded-2xl w-full mb-4" onChangeText={(e) => handleUserChange(e, "username")}/>
+        <TextInput placeholder="Anzeigename" placeholderTextColor="#888" className="bg-gray-800 text-white p-4 rounded-2xl w-full mb-4" onChangeText={(e) => handleUserChange(e, "displayname")}/>
+        {/* TODO: profilbild */}
       
       <TouchableOpacity
         onPress={handleAnonSignIn}
