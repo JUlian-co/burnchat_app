23b3abc7bac061c60db4f9ee5fbeb41d9a7f6a1a
JUlian-co <hi@juliansinn.de>
2026-05-26 15:20:08 +0200

    lil better looking user list

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -8,15 +8,20 @@ import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
 import { useAuthContext } from "@/hooks/use-auth-context";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Plus, Search } from "lucide-react-native";
+import { Plus, Search, X } from "lucide-react-native";
 import {supabase} from "@/lib/supabase"
 import { useState } from "react";
 
 export default function HomeScreen() {
   const { profile } = useAuthContext();
   const [users, setUsers] = useState([])
+  const [showUsers, setShowUsers] = useState(false)
 
   const searchUsers = async (name) => {
+    if (name.trim === "") {
+      return;
+    }
+
     // TODO: Wir sollten noch machen dass man sich selbst keine freundschaftsanfrage schicken kann
     const { data, error } = await supabase
       .from('users')
@@ -36,26 +41,44 @@ export default function HomeScreen() {
   }
 
   const requestFriend = async (friendId) => {
-    const { data, error} = await supabase.from("friendships").insert({ user_id: profile.id, friend_id: friendId, status: "accepted"}) // TODO: Status auf pending
+    console.log("Requesting friendship with user ID:", friendId);
+
+    // TODO: das select statement einbauen um zu schauen ob die nicht schon befreundet sind
+    const { error} = await supabase.from("friendships").insert({ user_id: profile.id, friend_id: friendId, status: "accepted"}) // TODO: Status auf pending
+
+    if (error) {
+      console.error("Error sending friend request: ", error);
+
+      if (error.code === "23505") { // Unique violation
+        console.log("friend req already exists")
+      }
+    } else {
+      console.log("Friend request sent successfully");
+    }
   }
 
   return (
     <SafeAreaView>
 
       <View className="flex flex-row items-center w-full">
-        <View className="flex-row items-center justify-center bg-gray-400 size-14">
-          <Search size={28} />
-        </View>
-        <TextInput placeholder="Freunde finden..." className="border border-gray-300 p-2 w-full h-full text-white" onChangeText={(e) => searchUsers(e)}/>
+        <TouchableOpacity onPress={() => setShowUsers((prev) => !prev)} className="flex-row items-center justify-center bg-gray-400 size-14">
+          {showUsers ? <X /> : <Search size={28} />}
+        </TouchableOpacity>
+
+        {showUsers && <TextInput placeholder="Freunde finden..." className="border border-gray-300 p-2 w-full h-full text-white" onChangeText={(e) => searchUsers(e)}/>}
       </View>
 
-      {users.map((u) => (
-        <View key={u.id} className="px-4 py-2 border-b border-gray-300 flex-row items-center justify-between w-full">
-          <Text className="text-lg font-bold text-white">@{u.username} ({u.display_name})</Text>
-
-          <TouchableOpacity className="p-4"><Plus size={16} color={"#fff"} onPress={() => requestFriend(u.id)} /></TouchableOpacity>
-        </View>
-      ))}
+      {showUsers && (
+        <>
+          {users.map((u) => (
+            <View key={u.id} className="px-4 py-2 border-b border-gray-300 flex-row items-center justify-between w-full">
+            <Text className="text-lg font-bold text-white">@{u.username} ({u.display_name})</Text>
+            
+            <TouchableOpacity className="p-4"><Plus size={16} color={"#fff"} onPress={() => requestFriend(u.id)} /></TouchableOpacity>
+            </View>
+          ))}
+        </>
+      )}
 
 
       <Text className="text-white">Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii</Text>
