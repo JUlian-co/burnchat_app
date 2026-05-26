d41ac8049eeb35b74148c3e24bc050d017223232
JUlian-co <hi@juliansinn.de>
2026-05-26 15:19:37 +0200

    new icons and dont show title

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -5,6 +5,7 @@ import { HapticTab } from '@/components/haptic-tab';
 import { IconSymbol } from '@/components/ui/icon-symbol';
 import { Colors } from '@/constants/theme';
 import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Camera, Images } from 'lucide-react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -19,15 +20,15 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color }) => <Images size={28} name="house.fill" color={color} />,
         }}
-      />
+        />
       <Tabs.Screen
-        name="explore"
+        name="photo"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color }) => <Camera size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
