7025f2330f4428c7dd66f26a699e07c4bf2c4367
JUlian-co <hi@juliansinn.de>
2026-07-04 22:37:45 +0200

    friends display

--- app/(tabs)/friends.jsx
+++ app/(tabs)/friends.jsx
@@ -3,14 +3,17 @@ import { useAuthContext } from "@/hooks/use-auth-context";
 import { Button, Text, TouchableOpacity, View } from "react-native";
 
 export default function FriendsScreen() {
-    const { friends } = useAuthContext()
+  const { friends } = useAuthContext();
 
+  return (
+    <SafeAreaView>
+      <Text>Friends Screen</Text>
 
-    return (
-        <SafeAreaView>
-            <Text>Friends Screen</Text>
-
-
-        </SafeAreaView>
-    )
+      {friends?.map((f) => (
+        <View key={f.id} className="flex-row items-center space-x-4 py-2">
+          <Text className="text-lg font-bold text-white">{f.displayname || f.username}</Text>
+        </View>
+      ))}
+    </SafeAreaView>
+  );
 }
